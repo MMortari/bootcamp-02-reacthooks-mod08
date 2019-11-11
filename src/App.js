3a2b6e36a3e557ddd19610547e699dc14086eac1
Mortari <bmortari2000@gmail.com>
2019-11-11 20:48:23 -0300

    Hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+/**
+ * useState
+ * Utilizado para a criação de variáveis de estado na aplicação
+ * const [techs, setTechs] = useState([]); // Techs iniciará com [];
+ */
 
 function App() {
-  return <h1>Hello Brother</h1>;
+  const [techs, setTechs] = useState(['React', 'React Native'])
+  const [newTech, setNewTech] = useState('')
+
+  function handleAdd() {
+    setTechs([ ...tech, newtech ]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <input type="text" onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Add</button>
+      <ul>
+        {techs.map(tech => {
+          <li key={tech}>{tech}</li>
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
